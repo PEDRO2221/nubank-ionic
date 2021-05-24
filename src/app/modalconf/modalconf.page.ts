22a0796c19d08e709e6488c7210f9d86c0436d6f
PEDRO2221 <anonomousdex01@gmail.com>
2021-05-24 20:01:33 -0300

    button config fucionando

--- src/app/modalconf/modalconf.page.ts
+++ src/app/modalconf/modalconf.page.ts
@@ -15,4 +15,16 @@ export class ModalconfPage implements OnInit {
   CloseModal(){
     this.modalController.dismiss();
   }
+  public options: Array<any> = [
+    { icon: "help-circle-outline", text: "Me ajuda" },
+    { icon: "barcode-outline", text: "Pagar" },
+    { icon: "person-add-outline", text: "Indicar amigos" },
+    { icon: "swap-vertical-outline", text: "Transferir" },
+    { icon: "download-outline", text: "Depositar" },
+    { icon: "cash-outline", text: "Empréstimo" },
+    { icon: "card-outline", text: "Cartão Virtual" },
+    { icon: "phone-portrait-outline", text: "Recarga de celular" },
+    { icon: "construct-outline", text: "Ajustar limite" },
+    { icon: "lock-closed-outline", text: "Bloquear Cartão" },    
+  ];
 }
