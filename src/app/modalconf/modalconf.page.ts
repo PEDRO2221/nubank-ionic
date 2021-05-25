eea7313c39309e7471d2482986f3ac3aac9bbc70
PEDRO2221 <anonomousdex01@gmail.com>
2021-05-24 22:32:10 -0300

    Configuração concluida v6

--- src/app/modalconf/modalconf.page.ts
+++ src/app/modalconf/modalconf.page.ts
@@ -17,14 +17,12 @@ export class ModalconfPage implements OnInit {
   }
   public options: Array<any> = [
     { icon: "help-circle-outline", text: "Me ajuda" },
-    { icon: "barcode-outline", text: "Pagar" },
-    { icon: "person-add-outline", text: "Indicar amigos" },
-    { icon: "swap-vertical-outline", text: "Transferir" },
-    { icon: "download-outline", text: "Depositar" },
-    { icon: "cash-outline", text: "Empréstimo" },
-    { icon: "card-outline", text: "Cartão Virtual" },
-    { icon: "phone-portrait-outline", text: "Recarga de celular" },
-    { icon: "construct-outline", text: "Ajustar limite" },
-    { icon: "lock-closed-outline", text: "Bloquear Cartão" },    
+    { icon: "person-outline", text: "Perfil" },
+    { icon: "construct-outline", text: "Configurar conta" },
+    { icon: "card-outline", text: "Configurar Cartão" },
+    { icon: "storefront-outline", text: "Pedir conta PJ" },
+    { icon: "mail-outline", text: "Configurar Nortificações" },
+    { icon: "phone-portrait-outline", text: "Configurações do app" },
+    { icon: "help-circle-outline", text: "Sobre" },    
   ];
 }
